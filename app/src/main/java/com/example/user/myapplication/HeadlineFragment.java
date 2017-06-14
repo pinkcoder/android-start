98593d533b26bc23c45bd8229c1317f381cdfc4f
user <user@users-MacBook-Pro-2.local>
2017-06-14 16:50:35 +0700

    First commit

---
+++ app/src/main/java/com/example/user/myapplication/HeadlineFragment.java
@@ -0,0 +1,17 @@
+package com.example.user.myapplication;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by user on 6/14/17.
+ */
+
+public class HeadlineFragment extends Fragment {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.news_article,container,false);
+    }
+}
